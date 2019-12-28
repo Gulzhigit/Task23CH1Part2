ee89a30b53e9c724de2e2f1c5cb13c81a8aaa7f7
Gulzhigit <mr.ksedos@mail.com>
2019-12-28 13:20:48 +0600

    23-commit

---
+++ task23.py
@@ -0,0 +1,12 @@
+year = int(input())
+if year % 4 != 0:
+    print("usual year")
+elif year % 100 == 0:
+    if year % 400 == 0:
+        print("intercalary year")
+    else:
+        print("usual year")
+else:
+    print("intercalary year")
+    
+    
